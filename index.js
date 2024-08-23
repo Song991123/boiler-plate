90d51a43e8b23e6990369dae2796c329e1e617c3
Song991123 <sjh11235678@gmail.com>
2024-08-23 22:37:29 +0900

    # 기본 기능 추가
    - 로그인 기능 추가
    - 로그인 시 토큰 발급

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import User from './models/User.js';
 import config from './config/key.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = 5000;
@@ -11,6 +12,8 @@ mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...')
 .catch(err => console.log(err));
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.get('/', (req, res) => res.send('Hello World!'));
 // 회원가입 기능
 app.post('/register', async (req, res) => {
@@ -22,5 +25,39 @@ app.post('/register', async (req, res) => {
         res.json({ success: false, err });
     }
 });
+
+app.post('/login', async (req, res) => {
+    try {
+        // 1. 요청된 이메일을 DB에서 찾기
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            });
+        }
+
+        // 2. 요청된 이메일이 DB에 있다면, 비밀번호가 맞는 비밀번호인지 확인
+        const isMatch = await user.comparePassword(req.body.password);
+
+        if (!isMatch) {
+            return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." });
+        }
+
+        // 3. 비밀번호가 일치하면 토큰 생성
+        const updatedUser = await user.generateToken();
+
+        // 클라이언트에게 토큰을 쿠키로 설정
+        res.cookie('x_auth', user.token)
+            .status(200)
+            .json({ loginSuccess: true, userId: user._id });
+
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
 
 app.listen(port, () => console.log(`Server running at http://127.0.0.1:${port}/`));
