50a1e716184fefab17d69910a542b049cc7ae0b7
Song991123 <sjh11235678@gmail.com>
2024-08-24 02:22:29 +0900

    # 기본 기능 추가 - 2.1
    - auth.js 주석 추가

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,11 +2,16 @@ import cookieParser from 'cookie-parser';
 import User from '../models/User.js';
 
 /**
- * @description 인증 처리를 하는 메서드
+ * @description 인증 처리를 하는 미들웨어 함수
+ * - 클라이언트의 요청에서 토큰을 추출하고, 유효성을 검사하여 인증 여부를 결정
+ * - 인증이 성공하면 요청 객체에 사용자 정보를 추가하고, 다음 미들웨어로 이동
+ * - 인증 실패 시 적절한 에러 응답을 클라이언트에 반환
  * 
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+ * @param {Object} req - Express 요청 객체
+ * @param {Object} res - Express 응답 객체
+ * @param {Function} next - 다음 미들웨어로 넘어가는 함수
+ * 
+ * @returns {void} - 미들웨어 함수는 반환값이 없음
  */
 export default async function auth(req, res, next){
     try{
