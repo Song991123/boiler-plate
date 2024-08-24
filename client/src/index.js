137e614185e345f0b37da200e09333e33c1e4ec5
Song991123 <sjh11235678@gmail.com>
2024-08-24 21:37:48 +0900

    fix: 기본 디자인 임포트문 수정
    - ant design 버젼 변경으로 인한 디자인  임포트문 수정

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'antd/dist/antd.css'; // Ant Design의 기본 CSS를 임포트
+import 'antd/dist/reset.css'; // Ant Design의 기본 CSS를 임포트
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
